6e01dc8577702328d582b2a3c3b75838ea9a259c
DianaRubiano16 <diacrubiano5@soy.sena.edu.co>
2024-10-13 00:21:38 -0500

    Elimina home.css y home.jsx

--- social-network/src/components/post.jsx
+++ social-network/src/components/post.jsx
@@ -1,27 +1,37 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import './post.css'; 
+import { FaThumbsUp, FaComment, FaShare } from 'react-icons/fa'; 
+import '../assets/styles/post.css'; 
 
-const Post = ({ post }) => {
-    return (
-        <div className="post">
-            <h2 className="post-title">{post.title}</h2>
-            <p className="post-content">{post.content}</p>
-            <div className="post-footer">
-                <span className="post-likes">{post.likes} Likes</span>
-                <span className="post-shares">{post.shares} Shares</span>
-            </div>
+const Post = ({ userId, id, title, body, likes, shares }) => {
+  return (
+    <div className="post" data-user-id={userId} data-id={id}>
+      <div className="post__header">
+        <img className="post__avatar" src="avatar.png" alt="User Avatar" />
+        <div className="post__user-info">
+          <span className="post__username">Friendly User</span>
+          <span className="post__timestamp">2 hours ago</span>
         </div>
-    );
-};
-
-Post.propTypes = {
-    post: PropTypes.shape({
-        title: PropTypes.string.isRequired,
-        content: PropTypes.string.isRequired,
-        likes: PropTypes.number.isRequired,
-        shares: PropTypes.number.isRequired,
-    }).isRequired,
+      </div>
+      <h2 className="post__title">{title}</h2>
+      <p className="post__body">{body}</p>
+      <div className="post__image-placeholder"></div>
+      <div className="post__stats">
+        <span className="post__likes">Likes: {likes}</span>
+        <span className="post__shares">Shares: {shares}</span>
+      </div>
+      <div className="post__actions">
+        <button className="post__action-button">
+          <FaThumbsUp /> Me Gusta
+        </button>
+        <button className="post__action-button">
+          <FaComment /> Comentar
+        </button>
+        <button className="post__action-button">
+          <FaShare /> Compartir
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
